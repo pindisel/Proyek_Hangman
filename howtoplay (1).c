dc3bcea8504e0922bd998b50774c9336b1436640
omarmoch13 <72249810+omarmoch13@users.noreply.github.com>
2020-10-12 08:55:22 -0700

    Add files via upload

---
+++ howtoplay (1).c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+    
+    
+int main()
+{    
+    printf ("           __    __   ______  ____    ____    ___                                      \n");                                                                                    
+    printf ("          |  |  |  | /  __  \\ \\   \\  /    \\  /  /                            \n");                                                                       
+    printf ("          |  |__|  | | |  | |  \\   \\/  /\\  \\/  /                                   \n"); 
+	printf ("          |   __   | | |__| |   \\     /  \\    /                       \n");                                                                                    
+    printf ("          |__|  |__| \\______/    \\___/    \\__/                                \n");                                                                       
+    printf ("                                                        \n");                                                                                    
+    printf ("                 __________    ______                                                   \n");                                                                       
+    printf ("                |___    ___|  /  __  \\                                         \n"); 
+	printf ("                    |  |      | |  | |                                     \n");                                                                                    
+    printf ("                    |  |      | |__| |                                    \n");                                                                       
+    printf ("                    |__|      \\______/                                          \n");   
+    printf ("                                                        \n");                                                                                    
+    printf ("     ________                     _____    ___      ___             \n");                                                                       
+    printf ("     |   __  \\   ___             /     \\   \\  \\    /  /           \n");
+    printf ("     |  |__| |  |  |            /   _   \\   \\  \\__/  /         \n");                                                                                    
+    printf ("     |  ____ /  |  |           /   / \\   \\   \\__  __/          \n");                                                                       
+    printf ("     |   |      |  |          /   /   \\   \\    |  |            \n");
+    printf ("     |   |      |  |_____    /   /     \\   \\   |  |                   \n");                                                                                    
+    printf ("     |___|      |________|  /___/       \\___\\  |__|                            \n");                                                                       
+   
+    printf ("                                                            \n");
+    printf ("   ________________________________________________________________      \n");
+    printf ("  / ______________________________________________________________ \\    \n");
+    printf (" / /                                                              \\ \\        \n");                                                              
+    printf (" | |                                                               | |             \n");                                                                                                             
+    printf (" | |    * in this gamewe provide a choice of 3 themes              | |                  \n");                                                                                                        
+    printf (" | |    * each theme has 8 types of words to string together       | |                       \n");                                                                                                   
+    printf (" | |                                                               | |                            \n");                                                                                              
+    printf (" | |    1. this game is a word stringing game                      | |                     \n");                                                                                         
+    printf (" | |                                                               | |                                      \n");                                                                                    
+    printf (" | |    2. You have to string together words to save               | |                                           \n");                                                                               
+    printf (" | |        the hanged man.                                        | |                       \n");                                                                          
+    printf (" | |                                                               | |                                                     \n");                                                                     
+    printf (" | |    3. You only have 3 lives.                                  | |                                    \n");                                                                
+    printf (" | |                                                               | |                                       \n");                                                           
+    printf (" | |    4. Every time you make a mistake, your life will be        | |                                           \n");                                                      
+    printf (" | |         reduced and the hanged will be hung even more.        | |         \n");                                                                                                                 
+    printf (" | |                                                               | |           \n");                                                                                                               
+    printf (" | |    5. If your life runs out, you lose.                        | |       \n");                                                                                                                   
+    printf (" | |                                                               | |      \n");                                                                                                                    
+    printf (" | |                                                               | |      \n");                                                                                                                    
+    printf (" | |                                                               | |      \n");                                                                                                                    
+    printf (" \\ \\______________________________________________________________/ /      \n");                                                             
+    printf ("  \\________________________________________________________________/\n");
+}

56a2ed9d639fdcd2a672507ac2a294230a4c369e
omarmoch13 <72249810+omarmoch13@users.noreply.github.com>
2020-10-12 11:22:19 -0700

    Add files via upload

---
+++ gambar.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+
+void three(){
+	printf ("     ________________________________________                                            \n");
+	printf ("    |    HANGMAN                             |           /\\/\\  /\\/\\  /\\/\\   \n");
+	printf ("    |    ____________________________________|           \\  /  \\  /  \\  /       \n");
+	printf ("    |   |                              \\   /              \\/    \\/    \\/          \n");
+	printf ("    |   |                               \\ /                         \n");
+	printf ("    |   |                                |                                  \n");
+	printf ("    |   |                                |                                 \n");
+	printf ("    |   |                                |                                  \n");
+	printf ("    |   |                               _|_                                  \n");
+	printf ("    |   |                              |___|                                   \n");
+	printf ("    |   |                              _____                                  \n");
+	printf ("    |   |                             | ^ ^ |                                \n");
+    printf ("    |   |                             |__>__|                                     \n");
+	printf ("    |   |                               _|_                                 \n");
+	printf ("    |   |                              / | \\                                  \n");
+	printf ("    |   |                             /  |  \\                                 \n");
+	printf ("    |   |                                |                                  \n");
+    printf ("    |   |                               / \\                                  \n");
+    printf ("    |   |                              /   \\                                  \n");
+    printf ("    |___|===================================================                                                                    \n");
+    printf ("                                                                            \n");
+    printf ("                                                                            \n");
+    printf ("                                                                            \n");
+}
+
+void two(){
+	printf ("     ________________________________________                                                                     \n");
+	printf ("    |    HANGMAN                             |         /\\/\\  /\\/\\                  \n");
+	printf ("    |    ____________________________________|         \\  /  \\  /                                               \n");
+	printf ("    |   |                              \\   /            \\/    \\/                  \n");
+	printf ("    |   |                               \\ /                            \n");
+	printf ("    |   |                                |                                \n");
+	printf ("    |   |                                |                                  \n");
+	printf ("    |   |                                |                                  \n");
+	printf ("    |   |                                |                                  \n");
+	printf ("    |   |                                |                                  \n");
+	printf ("    |   |                              __|__                                  \n");
+	printf ("    |   |                             | 0 0 |                                \n");
+    printf ("    |   |                             |_---_|                                     \n");
+	printf ("    |   |                               |_|                                  \n");
+	printf ("    |   |                              / | \\                                  \n");
+	printf ("    |   |                             /  |  \\                                 \n");
+	printf ("    |   |                                |                                  \n");
+    printf ("    |   |                               / \\                                  \n");
+    printf ("    |   |                              /   \\                                  \n");
+    printf ("    |___|===================================================                                                                    \n");
+    printf ("                                                                            \n");
+    printf ("                                                                            \n");
+    printf ("                                                                            \n");
+}
+
+void one (){
+
+	printf ("     ________________________________________                                                                     \n");
+	printf ("    |    HANGMAN                             |         /\\/\\                      \n");
+	printf ("    |    ____________________________________|         \\  /                                                 \n");
+	printf ("    |   |                              \\   /            \\/                    \n");
+	printf ("    |   |                               \\ /                            \n");
+	printf ("    |   |                                |                                \n");
+	printf ("    |   |                                |                                  \n");
+	printf ("    |   |                              __|__                                  \n");
+	printf ("    |   |                             | _ _ |                               \n");
+	printf ("    |   |                             |__+__|                               \n");
+	printf ("    |   |                               |_|                                 \n");
+	printf ("    |   |                              / | \\                           \n");
+    printf ("    |   |                             /  |  \\                             \n");
+	printf ("    |   |                                |                                 \n");
+	printf ("    |   |                               / \\                              \n");
+	printf ("    |   |                              /   \\                          \n");
+	printf ("    |   |                                                                  \n");
+    printf ("    |   |                                                             \n");
+    printf ("    |   |                                                                \n");
+    printf ("    |___|===================================================                                                                    \n");
+    printf ("                                                                            \n");
+    printf ("                                                                            \n");
+    printf ("                                                                            \n");
+}
+
+void sad();{
+	printf ("     ________________________________________                                                                     \n");
+	printf ("    |    HANGMAN                             |                               \n");
+	printf ("    |    ____________________________________|                                                         \n");
+	printf ("    |   |                              \\   /                              \n");
+	printf ("    |   |                               \\ /                            \n");
+	printf ("    |   |                                |                                \n");
+	printf ("    |   |                                |                                  \n");
+	printf ("    |   |    ==YOU LOSE==              __|__                                  \n");
+	printf ("    |   |    GO TO THE HELL           | X X |                               \n");
+	printf ("    |   |      BASTARD!!!             |_===_|                               \n");
+	printf ("    |   |                               |_|                                 \n");
+	printf ("    |   |                              / | \\                           \n");
+    printf ("    |   |                             /  |  \\                             \n");
+	printf ("    |   |                                |                                 \n");
+	printf ("    |   |                               / \\                              \n");
+	printf ("    |   |                              /   \\                          \n");
+	printf ("    |   |                                                                  \n");
+    printf ("    |   |                                                             \n");
+    printf ("    |   |                                                                \n");
+    printf ("    |___|===================================================                                                                    \n");
+    printf ("                                                                            \n");
+    printf ("                                                                            \n");
+    printf ("                                                                            \n");
+}
+
+void happy();{
+    printf ("     ________________________________________                                            \n");
+	printf ("    |    HANGMAN                             |             \n");
+	printf ("    |    ____________________________________|              \n");
+	printf ("    |   |                              \\   /                    \n");
+	printf ("    |   |                               \\ /                         \n");
+	printf ("    |   |                                |                                  \n");
+	printf ("    |   |  =YOU WIN=                     |                                 \n");
+	printf ("    |   |  LETS DANCE                    |                                  \n");
+	printf ("    |   |  TOGEHTHER                    _|_                                  \n");
+	printf ("    |   |                              |___|                                   \n");
+	printf ("    |   |                              _____                                  \n");
+	printf ("    |   |                             | ^ ^ |                                \n");
+    printf ("    |   |                             |_'>'_|                                     \n");
+	printf ("    |   |                               _|_                                 \n");
+	printf ("    |   |                         \\_/\\_/ |\\_/\\_/                                  \n");
+	printf ("    |   |                                |                                   \n");
+	printf ("    |   |                                |                                  \n");
+    printf ("    |   |                               / \\                                  \n");
+    printf ("    |   |                              /   \\                                  \n");
+    printf ("    |___|===================================================                                                                    \n");
+    printf ("                                                                            \n");
+    printf ("                                                                            \n");
+    printf ("                                                                            \n");  
+}

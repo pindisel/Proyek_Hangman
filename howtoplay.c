acad2f32d4adc80bee4fd6b0af48afc5ff3b67a4
omarmoch13 <72249810+omarmoch13@users.noreply.github.com>
2020-10-12 05:38:56 -0700

    Add files via upload

---
+++ howtoplay.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+    
+    
+int main()
+{    
+    printf ("           __    __   ______  ____    ____    ___                                      \n");                                                                                    
+    printf ("          |  |  |  | /  __  \\ \\   \\  /    \\  /  /                            \n");                                                                       
+    printf ("          |  |__|  | | |  | |  \\   \\/  /\\  \\/  /                                   \n"); 
+	printf ("          |   __   | | |__| |   \\     /  \\    /                       \n");                                                                                    
+    printf ("          |__|  |__| \\______/    \\___/    \\__/                                \n");                                                                       
+    printf ("                                                        \n");                                                                                    
+    printf ("                 __________    ______                                                   \n");                                                                       
+    printf ("                |___    ___|  /  __  \\                                         \n"); 
+	printf ("                    |  |      | |  | |                                     \n");                                                                                    
+    printf ("                    |  |      | |__| |                                    \n");                                                                       
+    printf ("                    |__|      \\______/                                          \n");   
+    printf ("                                                        \n");                                                                                    
+    printf ("     ________                     _____    ___      ___             \n");                                                                       
+    printf ("     |   __  \\   ___             /     \\   \\  \\    /  /           \n");
+    printf ("     |  |__| |  |  |            /   _   \\   \\  \\__/  /         \n");                                                                                    
+    printf ("     |  ____ /  |  |           /   / \\   \\   \\__  __/          \n");                                                                       
+    printf ("     |   |      |  |          /   /   \\   \\    |  |            \n");
+    printf ("     |   |      |  |_____    /   /     \\   \\   |  |                   \n");                                                                                    
+    printf ("     |___|      |________|  /___/       \\___\\  |__|                            \n");                                                                       
+   
+    printf ("                                                            \n");
+    printf ("   ________________________________________________________________      \n");
+    printf ("  / ______________________________________________________________ \\    \n");
+    printf (" / /                                                              \\ \\        \n");                                                              
+    printf (" | |                                                               | |             \n");                                                                                                             
+    printf (" | |   1.  choose the theme you want.                              | |                  \n");                                                                                                        
+    printf (" | |   2.  after that, sort the disappearing letters into a word.  | |                       \n");                                                                                                   
+    printf (" | |   3.  don't get the word wrong.                               | |                            \n");                                                                                              
+    printf (" | |   4.  if the wrong word is stringed together, the hanged      | |                     \n");                                                                                         
+    printf (" | |       person will die                                         | |                                      \n");                                                                                    
+    printf (" | |   5.  And you're going to lose.                               | |                                           \n");                                                                               
+    printf (" | |                                                               | |                       \n");                                                                          
+    printf (" | |                                                               | |                                                     \n");                                                                     
+    printf (" | |                                                               | |                                    \n");                                                                
+    printf (" | |                                                               | |                                       \n");                                                           
+    printf (" | |    ______________                                             | |                                           \n");                                                      
+    printf (" | |    | /          |                                             | |         \n");                                                                                                                 
+    printf (" | |    |/           |__                                           | |           \n");                                                                                                               
+    printf (" | |    |            |__|                                          | |       \n");                                                                                                                   
+    printf (" | |    |             |                                            | |      \n");                                                                                                                    
+    printf (" | |    |            /|\\                                           | |      \n");                                                                                                                    
+    printf (" | |    |            / \\                                           | |      \n");                                                                                                                    
+    printf (" \\ \\____|__________________________________________________________/ /      \n");                                                             
+    printf ("  \\________________________________________________________________/\n");
+}

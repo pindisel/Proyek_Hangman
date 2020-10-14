37c89ede4901a672611e0dc6384ab8e597378f9c
pindisel <72156714+pindisel@users.noreply.github.com>
2020-10-15 01:40:51 +0700

    Add files via upload

--- gambar.c
+++ gambar.c
@@ -3,26 +3,26 @@
 //Tampilan hangman disaat nyawa = 3
 void three()
 {
-	printf ("\t\t\t     ________________________________________       \n");
-	printf ("\t\t\t    |    HANGMAN                             |           /\\/\\  /\\/\\  /\\/\\   \n");
-	printf ("\t\t\t    |    ____________________________________|           \\  /  \\  /  \\  /       \n");
-	printf ("\t\t\t    |   |                              \\   /              \\/    \\/    \\/          \n");
-	printf ("\t\t\t    |   |                               \\ /                         \n");
-	printf ("\t\t\t    |   |                                |                                  \n");
-	printf ("\t\t\t    |   |                                |                                 \n");
-	printf ("\t\t\t    |   |                                |                                  \n");
-	printf ("\t\t\t    |   |                               _|_                                  \n");
-	printf ("\t\t\t    |   |                              |___|                                   \n");
-	printf ("\t\t\t    |   |                              _____                                  \n");
-	printf ("\t\t\t    |   |                             | ^ ^ |                                \n");
-    printf ("\t\t\t    |   |                             |__>__|                                     \n");
-	printf ("\t\t\t    |   |                               _|_                                 \n");
-	printf ("\t\t\t    |   |                              / | \\                                  \n");
-	printf ("\t\t\t    |   |                             /  |  \\                                 \n");
-	printf ("\t\t\t    |   |                                |                                  \n");
-    printf ("\t\t\t    |   |                               / \\                                  \n");
-    printf ("\t\t\t    |   |                              /   \\                                  \n");
-    printf ("\t\t\t    |___|===================================================        \n");
+	printf ("\t\t\t    "BROWN_BG"                                          "RESET"\n");
+	printf ("\t\t\t    "BROWN_BG"     HANGMAN                              "RESET"           /\\/\\  /\\/\\  /\\/\\   \n");
+	printf ("\t\t\t    "BROWN_BG"                                          "RESET"           \\  /  \\  /  \\  /       \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                              \\   /              \\/    \\/    \\/          \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                               \\ /                         \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                                |                                  \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                                |                                 \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                                |                                  \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                               _|_                                  \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                              |___|                                   \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                              _____                                  \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                             | ^ ^ |                                \n");
+    printf ("\t\t\t    "BROWN_BG"     "RESET"                             |__>__|                                     \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                               _|_                                 \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                              / | \\                                  \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                             /  |  \\                                 \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                                |                                  \n");
+    printf ("\t\t\t    "BROWN_BG"     "RESET"                               / \\                                  \n");
+    printf ("\t\t\t    "BROWN_BG"     "RESET"                              /   \\                                  \n");
+    printf ("\t\t\t    "BROWN_BG"     "RESET"===================================================        \n");
 }
 
 //Tampilan hangman disaat nyawa = 2
@@ -106,31 +106,31 @@ void dead()
 
 }
 
-
 //Tampilan hangman disaat nyawa tidak habis
 void alive()
 {
 
-    printf ("\t\t\t     ________________________________________      \n");
-	printf ("\t\t\t    |    HANGMAN                             |             \n");
-	printf ("\t\t\t    |    ____________________________________|              \n");
-	printf ("\t\t\t    |   |                              \\   /                    \n");
-	printf ("\t\t\t    |   |                               \\ /                         \n");
-	printf ("\t\t\t    |   |                                |                                  \n");
-	printf ("\t\t\t    |   |    =YOU WIN=                   |                                 \n");
-	printf ("\t\t\t    |   |    LETS DANCE                  |                                  \n");
-	printf ("\t\t\t    |   |     TOGETHER                  _|_                                  \n");
-	printf ("\t\t\t    |   |                              |___|                                   \n");
-	printf ("\t\t\t    |   |                              _____                                  \n");
-	printf ("\t\t\t    |   |                             | ^ ^ |                                \n");
-    printf ("\t\t\t    |   |                             |_'>'_|                                     \n");
-	printf ("\t\t\t    |   |                               _|_                                 \n");
-	printf ("\t\t\t    |   |                         \\_/\\_/ |\\_/\\_/                                  \n");
-	printf ("\t\t\t    |   |                                |                                   \n");
-	printf ("\t\t\t    |   |                                |                                  \n");
-    printf ("\t\t\t    |   |                               / \\                                  \n");
-    printf ("\t\t\t    |   |                              /   \\                                  \n");
-    printf ("\t\t\t    |___|===================================================    \n");
+    printf ("\t\t\t    "BROWN_BG"                                          "RESET"    \n");
+	printf ("\t\t\t    "BROWN_BG"     HANGMAN                              "RESET"             \n");
+	printf ("\t\t\t    "BROWN_BG"                                          "RESET"              \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                              \\   /                    \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                               \\ /                         \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                                |                                  \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"    =YOU WIN=                   |                                 \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"    LETS DANCE                  |                                  \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"     TOGETHER                  _|_                                  \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                              |___|                                   \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                              _____                                  \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                             | ^ ^ |                                \n");
+    printf ("\t\t\t    "BROWN_BG"     "RESET"                             |_'>'_|                                     \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                               _|_                                 \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                         \\_/\\_/ |\\_/\\_/                                  \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                                |                                   \n");
+	printf ("\t\t\t    "BROWN_BG"     "RESET"                                |                                  \n");
+    printf ("\t\t\t    "BROWN_BG"     "RESET"                               / \\                                  \n");
+    printf ("\t\t\t    "BROWN_BG"     "RESET"                              /   \\                                  \n");
+    printf ("\t\t\t    "BROWN_BG"     "RESET"===================================================    \n");  
 
 
 }
+
